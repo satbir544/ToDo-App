697d97ad237a2e8b50610797da9d82704981e885
Lsingh4673 <78841915+Lsingh4673@users.noreply.github.com>
2022-04-18 16:10:38 -0400

    updated about page

--- to-do-app/src/About.js
+++ to-do-app/src/About.js
@@ -5,7 +5,7 @@ function About() {
             <div>
                 <h3>About</h3>
                     <p1>
-                        This our ToDo App which help user to make thier day progressive and tension free. Basically there are four major section in the app.
+                        This our ToDo App which help user to make thier day progressive and tension free. Basically there are four major section in the app. 1. Home 2. Complete 3. Incomplete 4. History. Below it is describe each section breifly.
                     </p1>
                 </div>
 
@@ -20,21 +20,21 @@ function About() {
             <div>
                 <h3>Complete</h3>
                 <p1>
-                    This section contains all the task that are completed by user.
+                    This section contains all the task that are completed by user. When user create the task and complete the task by clicking on it, automaically the task comes to this section of application. If there is no task it show empty with a text message. 
                 </p1>
             </div>
 
             <div>
                 <h3>Incomplete</h3>
                 <p1>
-                    All Incompleted task is shown in this section.
+                    All Incompleted task is shown in this section. When user create the task it is already incompleted. Also, when user create the task is shown on Home page as well as in incomplete section. And when the tak is complete it automatically moves to complete section.
                 </p1>
             </div>
 
             <div>
                 <h3>History</h3>
                 <p1>
-                    This Section contain all tasks either it is complete or incomplete.
+                    This Section contain all tasks either it is complete or incomplete. 
                 </p1>
             </div>
         </div>
